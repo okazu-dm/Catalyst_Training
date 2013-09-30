b2f80815fe32727be6125515a8c125a7b6a8f969
Okada Kazuma <kokada@soba-project.com>
2013-09-30 16:41:36 +0900

    bug fix

--- lib/todoApp/Controller/Todos.pm
+++ lib/todoApp/Controller/Todos.pm
@@ -33,7 +33,21 @@ sub get :Local {
 }
 
 
-#REST api
+ sub object :Chained('base') :PathPart('id') :CaptureArgs(1) {
+        # $id = primary key of book to delete
+        my ($self, $c, $id) = @_;
+    
+        # Find the book object and store it in the stash
+        $c->stash(object => $c->stash->{resultset}->find($id));
+    
+        # Make sure the lookup was successful.  You would probably
+        # want to do something like this in a real app:
+        #   $c->detach('/error_404') if !$c->stash->{object};
+        die "TODO $id not found!" if !$c->stash->{object};
+    
+        # Print a message to the debug log
+        $c->log->debug("*** INSIDE OBJECT METHOD for obj id=$id ***");
+}
 
 sub create :Local {
 	my ($self, $c)=@_;
@@ -51,6 +65,20 @@ sub create :Local {
 
 }
 
+
+sub delete :Chained('object') :PathPart('delete') :Args(0) {
+        my ($self, $c) = @_;
+    
+        # Use the book object saved by 'object' and delete it along
+        # with related 'book_author' entries
+        $c->stash->{object}->delete;
+    
+        # Set a status message to be displayed at the top of the view
+        $c->stash->{status_msg} = "TODO deleted.";
+    
+        # Forward to the list action/method in this controller
+        $c->forward('get');
+}
 
 
 =encoding utf8
