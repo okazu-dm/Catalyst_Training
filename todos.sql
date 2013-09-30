ccc849c86d812884d6ff5bc2e24f7c97f462c540
Kenta Kase <kesin1202000@gmail.com>
2013-09-30 16:13:22 +0900

    デモ用のレコードを用意するように拡張

--- todos.sql
+++ todos.sql
@@ -8,3 +8,9 @@ CREATE TABLE todos (
 );
 
 INSERT INTO todos (name, comment) values ("testName", "testComment");
+INSERT INTO todos (name, comment, deadline) VALUES ('パーティの返事をする', '鹿鳴館', datetime('now', '+1 days'));
+INSERT INTO todos(name, comment) VALUES ('年金の書類を提出する', '両親に確認を取る');
+INSERT INTO todos(name, comment, deadline) VALUES ('内定式', 'スピーチの練習をしておく', datetime('now', '+1 days'));
+INSERT INTO todos(name, comment, deadline) VALUES ('澄川さんへのメッセージを書く', 'ありがとうございました！', datetime('now'));
+INSERT INTO todos(name, comment, deadline) VALUES ('借りた本を返す', '', datetime('now', '-1 days'));
+INSERT INTO todos(name, comment) VALUES ('Perlマスターになる', 'Rubyiestを駆逐してやる');
